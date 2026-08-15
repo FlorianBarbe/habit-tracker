1299346579f1525bbb995fb7c06af8191d5415dd
Florian Barbe <florian.barbe34@gmail.com>
2026-08-15 16:49:01 +0700

    Add interactive habit completion

--- app/page.tsx
+++ app/page.tsx
@@ -1,63 +1,69 @@
-import Image from "next/image";
+"use client";
+//Import de la fonction useState permettant de changer l'état de complétion de nos habitudes
+import { useState } from "react";
+
+//définition du type Habit
+type Habit = {
+  id: number;
+  name: string;
+  completed: boolean;
+};
+
+//déclaration des différents habitudes
+const initialHabits: Habit[] = [
+  { id: 1, name: "Read 20 minutes", completed: false },
+  { id: 2, name: "Exercise", completed: false },
+  { id: 3, name: "Meditate", completed: false },
+  { id: 4, name: "Take vitamins", completed: false },
+];
+
+
+
 
 export default function Home() {
+  //stockage des états de chaque habitude
+  const [habits, setHabits] = useState(initialHabits);
+
+  //fonctions
+  function toggleHabit(id: number) {
+    const updatedHabits = habits.map(
+      (habit) => {
+        if (habit.id === id) {
+          return { ...habit, completed: !habit.completed }; //...habit est la synthaxe spread qui crée une une copie de habit 
+        }
+        return habit;
+      });
+    //pour tester si le completed est bien modifié
+    console.table(updatedHabits);
+
+    setHabits(updatedHabits)
+  };
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
+
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-           Habit Tracker
+            Habit Tracker
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+          <h2 className="text-xl font-semibold tracking-tight text-black dark:text-zinc-50">
+            Today
+          </h2>
+          <ul>
+            {habits.map((habit) => (
+              <li key={habit.id}>
+                <label>
+                  <input type="checkbox"
+                    checked={habit.completed}
+                    //On rajoute un tick : dès qu'on change la vaeur de la case => appel de toggleHabit
+                    onChange={() => toggleHabit(habit.id)}
+                  />
+                  {habit.name}
+                </label>
+              </li>
+            ))}
+          </ul>
         </div>
       </main>
     </div>
